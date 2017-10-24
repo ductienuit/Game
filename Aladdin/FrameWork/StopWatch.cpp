acd764d57538db612fc1f16b3501863486f315b2
Duc Tien <ductien.uit@gmail.com>
2017-10-24 10:43:15 +0700

    [Add] Add Stop watch, đồng hồ đo giờ

---
+++ Aladdin/FrameWork/StopWatch.cpp
@@ -0,0 +1,72 @@
+
+#include "StopWatch.h"
+
+US_FRAMEWORK
+
+StopWatch::StopWatch()
+{
+	_stopwatch = 0.0f;
+	_isStart = false;
+	_isFinish = false;
+}
+
+StopWatch::~StopWatch()
+{
+}
+
+bool StopWatch::isFinish()
+{
+	return this->_isFinish;
+}
+// milisecond
+bool StopWatch::isTimeLoop(float time)
+{
+	float _totalgametime = GameTime::getInstance()->getTotalGameTime();
+	if (_isStart == false)
+	{
+		_stopwatch = time + _totalgametime;
+		_isStart = true;
+		return false;
+	}
+	float delta = _stopwatch - _totalgametime;
+	if (delta <= 0)
+	{
+		_stopwatch = time + delta + _totalgametime;
+		return true;
+	}
+	return false;
+}
+
+bool StopWatch::isStopWatch(float time)
+{
+	if (_isFinish == true)
+		return false;
+	float _totalgametime = GameTime::getInstance()->getTotalGameTime();
+
+	if (_isStart == false)
+	{
+		_stopwatch = time + _totalgametime;
+		_isStart = true;
+		return false;
+	}
+	float delta = _stopwatch - _totalgametime;
+	if (delta <= 0)
+	{
+		_isFinish = true;
+		return true;
+	}
+	return false;
+}
+void StopWatch::restart()
+{
+	_isStart = false;
+	_isFinish = false;
+}
+
+void StopWatch::timeLoopAction(float milisecond, pFunction action)
+{
+	if (isTimeLoop(milisecond))
+	{
+		(*action) ();
+	}
+}
---
+++ Aladdin/FrameWork/StopWatch.h
@@ -0,0 +1,41 @@
+
+#ifndef __STOP_WATCH__
+#define __STOP_WATCH__
+
+#include "GameTime.h"
+
+NS_FRAMEWORK
+NS_FRAMEWORK_BEGIN
+
+/*
+now we have a type are called pFunction. It's just a type stores address of function.
+example:
+pFunction pF = &RandomFunction;
+or
+pFunction pF = &ClassName::Method
+
+With RandomFunction and Method is a name of one function or method have no return value and argument
+*/
+
+typedef void(*pFunction)(void);
+
+class StopWatch
+{
+public:
+	StopWatch();
+	~StopWatch();
+
+	bool isFinish();
+	bool isTimeLoop(float time);
+	bool isStopWatch(float time);
+	void timeLoopAction(float milisecond, pFunction action);
+	void restart();
+private:
+	float	_stopwatch;
+	bool	_isStart;
+	bool	_isFinish;
+};
+
+NS_FRAMEWORK_END
+
+#endif // !__STOP_WATCH__
