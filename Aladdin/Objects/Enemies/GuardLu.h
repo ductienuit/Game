07e234a5654a212cb566618b2c21f42fbb07603d
huy031197 <33492436+huy031197@users.noreply.github.com>
2017-12-04 08:50:06 +0700

    ...

---
+++ Aladdin/Objects/Enemies/GuardLu.h
@@ -0,0 +1,51 @@
+
+#ifndef _GUARDSHORT_H
+#define _GUARDSHORT_H
+
+#include "../../FrameWork/Animation.h"
+#include "../../FrameWork/StopWatch.h"
+#include "../../FrameWork/IComponent.h"
+#include "../../FrameWork/Text.h"
+#include "BaseEnemy.h"
+using namespace std;
+
+#define GUARDLU_SPEED 30
+#define GUARDLU_SCORE 100
+
+class GuardLu : public BaseEnemy
+{
+public:
+	GuardLu(eStatus status, int posX, int posY, eDirection direction);
+
+	void InIt() override;
+	void Update(float deltatime) override;
+	void Draw(LPD3DXSPRITE, Viewport*) override;
+	void Release() override;
+
+	void onCollisionBegin(CollisionEventArg*);
+	void onCollisionEnd(CollisionEventArg*);
+	float checkCollision(BaseObject*, float);
+
+	float distanceBetweenAladdin();
+	void UpdateStatus(float dt);
+
+	IComponent* getComponent(string componentName);
+
+	Text* text;
+	~GuardLu();
+private:
+	void movingLeft();
+	void movingRight();
+	void standing();
+	void attack();
+
+	map<string, IComponent*> _listComponent;
+	map<int, Animation*> _animations;
+	Sprite *_divingSprite;
+	vector<BaseObject*> _listobject;
+	vector<IControlable*> _listControlObject;
+	bool _canThrow;
+};
+#endif
+
+
--- Aladdin/Objects/Scenes/PlayScene.h
+++ Aladdin/Objects/Scenes/PlayScene.h
@@ -18,6 +18,7 @@
 #include"../GameObjects/Objects/Land.h"
 #include"../Enemies/GuardThin.h"
 #include"../Enemies/GuardShort.h"
+#include"../Enemies/GuardLu.h"
 #include"../Enemies/Knife.h"
 #include"BackGround.h"
 using namespace std;
